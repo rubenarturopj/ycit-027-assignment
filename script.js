9aa52b1f850834259ab6cafa5daa09d72e1880bc
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2022-12-01 13:29:57 -0500

    fixed

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
+const catBtn = document.getElementById("cat-btn")
+const thisHasPic = document.querySelector(".image-container");
+const catPic = document.getElementById("catImage");
+
+catBtn.addEventListener("click", function getCatImage() {
+    if (catPic.style.visibility !== "visible") {
+        catPic.style.visibility = "visible"
+    }
+
+    return fetch("https://api.thecatapi.com/v1/images/search")
         .then((response) => {
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
+            return response.json();
         })
         .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
+            // console.log(data[0].url);
+            catPic.style.width = "500px";
+            catPic.style.height = "350px";
+            catPic.src = data[0].url;
+            // document.getElementById("catImage").src = data[0].url;
         });
-}
+});
